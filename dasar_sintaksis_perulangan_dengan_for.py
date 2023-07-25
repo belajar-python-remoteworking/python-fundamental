35c80727dd0dbc1942dc5607f1d34483d37ba63e
Raiha <raihandsyahendra@gmail.com>
2023-07-25 08:57:16 +0700

    perulangan dengan for

---
+++ dasar_sintaksis_perulangan_dengan_for.py
@@ -0,0 +1,14 @@
+"""
+Ini sintaksis perluangan program perulangan membaca buku
+"""
+
+Jumlah_buku=10
+print('Ibu memberi perintah : "Baca semua bukumu"')
+
+jumlah_buku_yg_sudah_dibaca = 0
+print(f"Jumlah buku yg sudah dibaca: {jumlah_buku_yg_sudah_dibaca}")
+
+for jumlah_buku_yg_sudah_dibaca in range (1,Jumlah_buku+1):
+    print(f"buku ke {jumlah_buku_yg_sudah_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yg sudah dibaca: {jumlah_buku_yg_sudah_dibaca}")
